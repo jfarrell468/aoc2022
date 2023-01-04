691c5fe30da9f59edbbef3095d7ca285235d679c
James Farrell <jamesfarrell@google.com>
2023-01-04 10:16:21 -0600

    Part 1 correct.

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let mut most_calories: i32 = 0;
+    let mut current_calories: i32 = 0;
+    for line in std::io::stdin().lines() {
+        let line = line.unwrap();
+        if line.is_empty() {
+            if current_calories > most_calories {
+                most_calories = current_calories;
+            }
+            current_calories = 0;
+            continue
+        }
+        current_calories += line.parse::<i32>().unwrap();
+    }
+    if current_calories > most_calories {
+        most_calories = current_calories;
+    }
+    println!("{}", most_calories)
 }
