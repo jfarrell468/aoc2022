981f691e93308b33f61e4b9795eee01bae70e098
James Farrell <jamesfarrell@google.com>
2023-01-04 10:22:59 -0600

    Part 2 correct.

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,19 +1,17 @@
 fn main() {
-    let mut most_calories: i32 = 0;
+    let mut calories = Vec::new();
     let mut current_calories: i32 = 0;
     for line in std::io::stdin().lines() {
         let line = line.unwrap();
         if line.is_empty() {
-            if current_calories > most_calories {
-                most_calories = current_calories;
-            }
+            calories.push(current_calories);
             current_calories = 0;
             continue
         }
         current_calories += line.parse::<i32>().unwrap();
     }
-    if current_calories > most_calories {
-        most_calories = current_calories;
-    }
-    println!("{}", most_calories)
+    calories.push(current_calories);
+    calories.sort_by(|a, b| b.cmp(a));
+    println!("Part 1: {}", calories[0]);
+    println!("Part 2: {}", calories[0] + calories[1] + calories[2])
 }
